9be0d57470c8c326f18611c838c1b87502c6d692
MJzamanifard <javadzamanii.1374@gmail.com>
2025-03-15 00:05:50 -0700

    Update tests.py

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
 
-# Create your tests here.
+class BlogTest(TestCase):
+
+    # Making sure a page is created by urls and name page
+    def test_home_page_by_url(self):
+        response = self.client.get('')
+        self.assertEqual(response.status_code, 200)
+
+    def test_home_page_by_name(self):
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
+
+    # Making sure something exists on a page
+    def test_exist_something_on_home_page(self):
+        response = self.client.get(reverse('home'))
+        self.assertContains(response, str('Login'))
+
+    def test_exist_something_on_login_page(self):
+        response = self.client.get(reverse('login'))
+        self.assertContains(response, str('Password reset'))
